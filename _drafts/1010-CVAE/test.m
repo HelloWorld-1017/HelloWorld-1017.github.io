924d5f06b79482f89bd90d85356eb81bb489b182
QinghuaMa <3392550566@qq.com>
2022-10-11 09:37:34 +0800

    Update-221011

--- _drafts/1010-CVAE/test.m
+++ _drafts/1010-CVAE/test.m
@@ -1,34 +1,6 @@
-clc, clear, close all
-% 
-% [xTrainImages, tTrain] = digitTrainCellArrayData;
-% xTrainImages = xTrainImages';
-% [Labels, ~] = find(tTrain == 1);
-% Labels(Labels==10) = 0;
-% 
-% 
+numLatentChannels = 16;
+numValidationImagesPerClass = 5;
+numClasses = 10;
 
-
-
-auimds = augmentedImageDatastore([100, 100], digitDatastore);% 注意这个28！！
-mbq = minibatchqueue(auimds, ...
-    MiniBatchSize=128, ...
-    PartialMiniBatch="discard", ...
-    MiniBatchFcn=@preprocessData, ...
-    MiniBatchFormat=["SSCB", "BC"]); % Attention here!!!
-A = read(auimds);
-head(A,2)
-
-[X, L] = next(mbq);
-
-figure
-fig = extractdata(X(:, :, 1, 1));
-image(fig)
-
-function [X,T] = preprocessData(XCell,TCell)
-% Extract image data from cell and concatenate
-X = cat(4, XCell{:});
-% Extract label data from cell and concatenate
-T = cat(1, TCell{:});
-% Rescale the images in the range [-1 1].
-X = rescale(X, 0, 1);
-end
+ZValidation = randn(numLatentChannels, numValidationImagesPerClass*numClasses,"single");
+TValidation = single(repmat(1:numClasses, [1 numValidationImagesPerClass]));
--- _drafts/1010-CVAE/test3.m
+++
@@ -1,4 +0,0 @@
-clc, clear, close all
-
-
-[xTrainImages, tTrain] = digitTrainCellArrayData;
